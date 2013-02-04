c58e1476c03c74d5935a8891405623988ef09fbb
Twiebie <twiebie@twiebie.com>
2013-02-04 18:32:37 +0100

    Russian language file

---
+++ barebones/barebones.ru.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Barebones Russian language
+ *
+ * @package Cotonti
+ * @version 0.9.0
+ * @author Twiebie
+ * @copyright Copyright (c) 2013 Cotonti Team, Twiebie
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$L['Information']  = 'Информация';
+$L['PostedOn']     = 'Размещено';
+$L['In']           = 'в';
+$L['Moderation']   = 'Модерация';
+$L['MembersOnly']  = 'Только для зарегистрированных пользователей';
+$L['RecentPages']  = 'Последние страницы';
+$L['RecentTopics'] = 'Последние обсуждения';
